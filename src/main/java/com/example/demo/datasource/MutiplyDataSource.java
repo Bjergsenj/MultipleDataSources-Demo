7630f8d861a4abbd55e2e447ba1b6431079c94f2
niemingxin <2424629998@qq.com>
2020-03-10 23:33:23 +0800

    添加mybatis-plus 分页插件

--- src/main/java/com/example/demo/datasource/MutiplyDataSource.java
+++ src/main/java/com/example/demo/datasource/MutiplyDataSource.java
@@ -2,8 +2,10 @@ package com.example.demo.datasource;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.baomidou.mybatisplus.core.MybatisConfiguration;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
 import com.example.demo.common.DataSourceType;
+import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.type.JdbcType;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -71,6 +73,11 @@ public class MutiplyDataSource {
         return new DataSourceTransactionManager(dynamicDataSource());
     }
 
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
     @Bean("sqlSessionFactory")
     public SqlSessionFactory sqlSessionFactory() throws Exception {
         MybatisSqlSessionFactoryBean sqlSessionFactory = new MybatisSqlSessionFactoryBean();
@@ -82,10 +89,11 @@ public class MutiplyDataSource {
         configuration.setMapUnderscoreToCamelCase(true);
         configuration.setCacheEnabled(false);
         sqlSessionFactory.setConfiguration(configuration);
-//        sqlSessionFactory.setPlugins(new Interceptor[]{ //PerformanceInterceptor(),OptimisticLockerInterceptor()
-//                paginationInterceptor() //添加分页功能
-//        });
-        //sqlSessionFactory.setGlobalConfig(globalConfiguration());
+        sqlSessionFactory.setPlugins(new Interceptor[]{ //PerformanceInterceptor(),OptimisticLockerInterceptor()
+                paginationInterceptor() //添加分页功能
+        });
+//        sqlSessionFactory.setGlobalConfig(globalConfiguration());
         return sqlSessionFactory.getObject();
     }
+
 }
